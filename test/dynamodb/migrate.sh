386bbb823bd3e030ec929c31d2e1957846c4bb1a
Dave Bush <dave.bush@windriver.com>
2024-12-31 08:46:55 -0500

    Added bootstrap test environment for dynamodb
    Signed-off-by: Dave Bush <dave.bush@windriver.com>

---
+++ test/dynamodb/migrate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+aws dynamodb describe-limits --endpoint-url http://dynamodb-local:8000 --region us-east-1
+aws dynamodb create-table --cli-input-json file://todo.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
+aws dynamodb create-table --table-name "todo-table-empty-1" --cli-input-json file://todo.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
+aws dynamodb create-table --table-name "todo-table-empty-2" --cli-input-json file://todo.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
+aws dynamodb create-table --table-name "todo-table-empty-3" --cli-input-json file://todo.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
