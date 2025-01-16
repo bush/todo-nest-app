5928e0e78182b5f2854143453117f4969d48201d
Dave Bush <davebush@gmail.com>
2025-01-16 05:57:39 -0500

    Added delay to migrate script.
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- test/dynamodb/migrate.sh
+++ test/dynamodb/migrate.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+echo "waiting ..."
+sleep 5
+echo "starting migration ..."
+
 aws dynamodb describe-limits --endpoint-url http://dynamodb-local:8000 --region us-east-1
 aws dynamodb create-table --cli-input-json file://todo.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
 aws dynamodb create-table --table-name "todo-table-empty-1" --cli-input-json file://todo.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
 aws dynamodb create-table --table-name "todo-table-empty-2" --cli-input-json file://todo.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
 aws dynamodb create-table --table-name "todo-table-empty-3" --cli-input-json file://todo.json --endpoint-url http://dynamodb-local:8000 --region us-east-1
+
+echo "migration complete"
